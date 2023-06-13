8aec451b3ca7f6d8a5761567d8bd47db96139259
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-06-13 16:34:22 +0300

    update imgGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import css from './ImageGallery.module.css';
 
 import ImageGalleryItem from 'components/ImageGalleryItem';
-import { RotatingLines } from 'npm i react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
 import { Button } from 'components/Button/Button';
 import Modal from 'components/Modal/Modal';
 import PropTypes from 'prop-types';
@@ -24,7 +24,7 @@ export default function ImageGallery({ searchByInputData }) {
       return;
     }
 
-    //якщо пропс відміний від наших даних робимо запит на сервер
+    //якщо пропс відмінний від наших даних робимо запит на сервер
     if (searchByInputData !== getImgApi.search) {
       getImgApi.resetPage();
 
@@ -39,7 +39,7 @@ export default function ImageGallery({ searchByInputData }) {
       }
       return;
     }
-    //якщо ми натиснули на кнопку loadMore йдемо по наступну порцію фотографій
+    //якщо ми натиснули на кнопку loadMore підгружаємо наступні фото
     if (buttonLoader) {
       try {
         getImgApi.setSearch(searchByInputData);
@@ -62,113 +62,104 @@ export default function ImageGallery({ searchByInputData }) {
   }, [searchByInputData, gallery, buttonLoader]);
 
   // Перегортаєм на наступну сторінку та замість кнопки ставим завантаження
-
   const handleAddImg = () => {
     getImgApi.addPage();
     setButtonLoader(true);
   };
 
   // відкриває/закриває модалку
-  toglleModal = () => {
-    this.setState(prevState => ({
-      openModal: !prevState.openModal,
-    }));
+  const toglleModal = () => {
+    setOpenModal(!openModal);
   };
-
   // зберігає індекс обєкта на якого click на розміченій сторінці
-  addActiveImg = index => {
-    this.setState({
-      activeImg: index,
-    });
+  const addActiveImg = index => {
+    setActiveImg(index);
 
     // відкриваєм модалку по кліку
-    this.toglleModal();
+    toglleModal();
   };
 
-  render() {
-    const { status, galery, buttonLoader, activeImg, openModal } = this.state;
-    // якщо зашли на сайт перший раз
-    if (status === 'idle') {
+  // render() {
+  //   const { status, gallery, buttonLoader, activeImg, openModal } = this.state;
+  // якщо зайшли в перший раз на сайт
+  if (status === 'idle') {
+    return (
+      <h2 style={{ textAlign: 'center', marginTop: 50 }}>
+        Введіть значення в поле пошуку
+      </h2>
+    );
+  }
+  // коли ввели і підвантажуються данні
+  if (status === 'pending') {
+    return (
+      <RotatingLines
+        className={css.Loader}
+        strokeColor=" #303f9f"
+        animationDuration="0.9"
+        width="40"
+      />
+    );
+  }
+  // малюємо галерею
+  if (status === 'resolved') {
+    // якщо прийшов порожній список повідомляєм що нічого не знайдено. Данний API при незнайденних даних не повертає 404,
+    // а повертає пустий масив
+    if (gallery.length === 0) {
       return (
-        <h2 style={{ textAlign: 'center', marginTop: 50 }}>
-          Введіть значення у поле пошуку
+        <h2 style={{ textAlign: 'center' }}>
+          За запитом "{searchByInputData}" нічого не знайдено
         </h2>
       );
-    }
-    // коли ввели і завантажуються дані
-    if (status === 'pending') {
+      // якщо в повернутому масиві є хоч 1 обєкт, малюємо нашу галерею
+    } else {
+      //якщо масив не порожній робим map по масиву обєктів та рендеримо перші <= 12 елементів
       return (
-        <RotatingLines
-          className={css.Loader}
-          strokeColor=" #303f9f"
-          animationDuration="0.9"
-          width="40"
-        />
+        <>
+          <h2 style={{ textAlign: 'center' }}>
+            Результат за запитом:{' '}
+            <span style={{ color: '#3f51b5' }}>{searchByInputData}</span>
+          </h2>
+          <ul className={css.ImageGallery}>
+            {gallery.map(({ id, webformatURL, title }, index) => (
+              <ImageGalleryItem
+                onChange={() => addActiveImg(index)}
+                className={css.loader}
+                key={id}
+                src={webformatURL}
+                alt={title}
+                index={index}
+              />
+            ))}
+          </ul>
+
+          {/* якщо масив має менше 12 обєктів не рендерим кнопку  */}
+          {/* коли догружаєм нову порцію картинок рендерим замість кнопки loader */}
+          {!buttonLoader ? (
+            gallery.length % 12 === 0 ? (
+              <Button onChange={handleAddImg}></Button>
+            ) : null
+          ) : (
+            <RotatingLines
+              className={css.Loader}
+              strokeColor=" #303f9f"
+              animationDuration="0.9"
+              width="40"
+            />
+          )}
+          {/* рендер модалки */}
+          {openModal && (
+            <Modal
+              toglleModal={toglleModal}
+              largeImageURL={gallery[activeImg]}
+            />
+          )}
+        </>
       );
     }
-
-    // малюємо галерею
-    if (status === 'resolved') {
-      // якщо прийшов порожній список повідомляєм що нічого не знайдено. Данний API при незнайденних даних не повертає 404, а повертає пустий масив
-      if (galery.length === 0) {
-        return (
-          <h2 style={{ textAlign: 'center' }}>
-            За Вашим запитом "{this.props.searchByInputData}" нічого не знайдено
-          </h2>
-        );
-        // якщо в повернутому масиві є хоть 1 обєкт, малюємо нашу галерею
-      } else {
-        //якщо масив не порожній робим map по масиву обєкті та рендеримо перші <= 12 елементів
-        return (
-          <>
-            <h2 style={{ textAlign: 'center' }}>
-              Результат за запитом:{' '}
-              <span style={{ color: '#3f51b5' }}>
-                {this.props.searchByInputData}
-              </span>
-            </h2>
-            <ul className={css.ImageGallery}>
-              {galery.map(({ id, webformatURL, title }, index) => (
-                <ImageGalleryItem
-                  onChange={() => this.addActiveImg(index)}
-                  className={css.loader}
-                  key={id}
-                  src={webformatURL}
-                  alt={title}
-                  index={index}
-                />
-              ))}
-            </ul>
-
-            {/* якщо масив має менше 12 обєктів не рендерим кнопку  */}
-            {/* коли догружаєм нову порцію картинок рендерим замість кнопки loader */}
-            {!buttonLoader ? (
-              galery.length % 12 === 0 ? (
-                <Button onChange={this.handleAddImg}></Button>
-              ) : null
-            ) : (
-              <RotatingLines
-                className={css.Loader}
-                strokeColor=" #303f9f"
-                animationDuration="0.9"
-                width="40"
-              />
-            )}
-            {/* рендер модалки */}
-            {openModal && (
-              <Modal
-                toglleModal={this.toglleModal}
-                largeImageURL={galery[activeImg]}
-              />
-            )}
-          </>
-        );
-      }
-    }
-    // якщо виникла помилка
-    if (status === 'rejected') {
-      return <h2>Виникла невідома помилка повторіть пошук ще раз</h2>;
-    }
+  }
+  // якщо виникла помилка
+  if (status === 'rejected') {
+    return <h2>Виникла помилка. Повторіть спробу, будь ласка</h2>;
   }
 }
 
