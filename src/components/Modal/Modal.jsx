11c5c4a03fcb48061196b052c06aa77ad1067805
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-06-13 15:35:05 +0300

    update modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,62 +1,67 @@
 import css from './Modal.module.css';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  state = {
-    returnSkeleton: true,
-    scelet: true,
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDon);
-  }
+export default function Modal({ largeImageURL, toglleModal }) {
+  // const [returnSkeleton, setReturnSceleton] = useState(true);
+  const [scelet, setScelet] = useState(true);
+
+  // state = {
+  //   returnSkeleton: true,
+  //   scelet: true,
+  // };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return function cleanup() {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  // useEffect(() => {
+  //   window.removeEventListener('keydown', handleKeyDown);
+  //   window.removeEventListener('load', showSceleton);
+  // });
 
   showSceleton = () => {
-    this.setState({ scelet: false });
+    setScelet(false);
   };
 
-  handleKeyDon = e => {
-    if (e.code === 'Escape') {
-      this.props.toglleModal();
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      toglleModal();
     }
   };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDon);
-    window.removeEventListener('load', this.showSceleton);
-  }
-
-  overlayClick = e => {
+  const overlayClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.toglleModal();
+      toglleModal();
     }
   };
-  render() {
-    const { largeImageURL } = this.props;
-    return createPortal(
-      <div className={css.Overlay} onClick={this.overlayClick}>
-        <div className={css.Modal}>
-          <button
-            className={css.Button}
-            type="button"
-            onClick={this.props.toglleModal}
-          ></button>
-          {this.state.scelet && <div className="Scelet"></div>}
-          <img
-            onLoad={this.showSceleton}
-            width={900}
-            height={600}
-            src={largeImageURL.largeImageURL}
-            alt={largeImageURL.tags}
-          />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+
+  return createPortal(
+    <div className={css.Overlay} onClick={overlayClick}>
+      <div className={css.Modal}>
+        <button
+          className={css.Button}
+          type="button"
+          onClick={toglleModal}
+        ></button>
+        {scelet && <div className="Scelet"></div>}
+        <img
+          onLoad={showSceleton}
+          width={900}
+          height={600}
+          src={largeImageURL.largeImageURL}
+          alt={largeImageURL.tags}
+        />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
 
 Modal.propTypes = {
